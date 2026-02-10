e28fc7476e3f43e500086e4838f45606e3fa4946
cristian2006xd <cristianamagua0@gmail.com>
2026-02-10 07:04:41 -0500

    cambio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,15 @@ import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
 import { Acerca } from './features/acerca/acerca';
 import { Consultas } from './features/consultas/consultas';
+import { Mascotas } from './shared/mascotas/mascotas';
 
 export const routes: Routes = [
     //1. Ruta Inicial
     {path: '', component: Home},
     //2. Rutas de navegación
     {path: 'acerca', component: Acerca},
-    {path: 'consultas', component: Consultas}
+    {path: 'consultas', component: Consultas},
+    {path: 'mascotas', component: Mascotas}
     //3. Redireccion si el usuario escribe una url que no existe
     //{path:'', component:Pagina404}
 ];
